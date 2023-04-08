e890b7a10702590e114b5e58b887502b721d9a7a
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-04-09 00:09:15 +0600

    create tshirt-mart react project

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./components/Home/Home";
+import Main from "./components/Layout/Main";
+import OrderReview from "./components/OrderReview/OrderReview";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: () => fetch("tshirts.json"),
+      },
+      {
+        path: "review",
+        element: <OrderReview />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

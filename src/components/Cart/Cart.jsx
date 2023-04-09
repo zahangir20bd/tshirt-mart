87088dc7fe7c9e9e595f7db5a774281673d18a9f
Zahangir Alam Rafe <rafe.z@standard-group.com>
2023-04-09 15:37:50 +0600

    product add and Delete from cart done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Cart = () => {
+const Cart = ({ cart, handleRemoveFromCart }) => {
+  console.log(cart);
   return (
     <div>
-      <h2>Order Summary:</h2>
+      <h2>Order Summary: {cart.length}</h2>
+      {cart.map((tshirt) => (
+        <p key={tshirt._id}>
+          {tshirt.name}{" "}
+          <button onClick={() => handleRemoveFromCart(tshirt._id)}>x</button>
+        </p>
+      ))}
     </div>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,14 +3,27 @@ import { useLoaderData } from "react-router-dom";
 import Tshirt from "../Tshirt/Tshirt";
 import "./Home.css";
 import Cart from "../Cart/Cart";
+import toast from "react-hot-toast";
 
 const Home = () => {
   const tshirts = useLoaderData();
   const [cart, setCart] = useState([]);
 
   const handleAddToCart = (tshirt) => {
-    console.log(tshirt);
+    const isHasCart = cart.find((ts) => ts._id === tshirt._id);
+    if (isHasCart) {
+      toast("Already Added");
+    } else {
+      const newCart = [...cart, tshirt];
+      setCart(newCart);
+    }
   };
+
+  const handleRemoveFromCart = (id) => {
+    const remaining = cart.filter((ts) => ts._id !== id);
+    setCart(remaining);
+  };
+
   return (
     <div className="home-container">
       <div className="t-shirts-container">
@@ -23,7 +36,7 @@ const Home = () => {
         ))}
       </div>
       <div className="cart-container">
-        <Cart></Cart>
+        <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
       </div>
     </div>
   );
